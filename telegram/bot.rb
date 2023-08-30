b6e2ad2b5bc3a0e959ffb27cc49713ae59e1f488
Anton Plaksin <aplaksin2000@gmail.com>
2023-08-30 16:45:47 +0400

    Naming almost works

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -2,6 +2,8 @@ require File.expand_path('../config/environment', __dir__)
 require 'telegram/bot'
 require 'pry'
 require 'launchy'
+require 'uri'
+require 'net/http'
 
 TOKEN = '6571102624:AAG4or30Lk0mbuW2ZvKqaqZY2QMi2F5FVUw'
 
@@ -13,7 +15,13 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
 		if message.text == '/show'
     	bot.api.send_message(chat_id: message.chat.id, text: 'Hi, pidor ;)')
    	elsif message.text =~ /https:\/\/www\.youtube\.com/
-   		Launchy.open(message.text)
+   		# Launchy.open(message.text)
+   		uri = URI(message.text)
+   		# req = Net::HTTP::Get.new(uri)
+   		response = Net::HTTP.get(uri)
+   		track_name = response[/meta\s?name\s?=\s?.title.\s?content\s?=\s?"(.*?)"/, 1]
+   		binding.pry
+   		Track.create(name: track_name, url: message.text) unless Track.find_by(url: message.text)
    	else
     	bot.api.send_message(chat_id: message.chat.id, text: 'Я тебя не понимаю :(')
    	end

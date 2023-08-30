6cf5837161ef58d0cdb090c8258f276e880fac80
Anton Plaksin <aplaksin2000@gmail.com>
2023-08-30 18:08:21 +0400

    Working name system

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -7,21 +7,23 @@ require 'net/http'
 
 TOKEN = '6571102624:AAG4or30Lk0mbuW2ZvKqaqZY2QMi2F5FVUw'
 
+def get_track_name(url)
+  response = Net::HTTP.get(URI(url))
+  response[/name\s?=\s?.title.\s?content\s?=\s?"(.*?)"/, 1].force_encoding("UTF-8")
+end
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
 	bot.listen do |message|
 		u_data = message.from
-		# binding.pry
 		user = User.find_by(telegram_id: u_data.id) || User.create(name: u_data.username, telegram_id: u_data.id, step: 0)
+
 		if message.text == '/show'
     	bot.api.send_message(chat_id: message.chat.id, text: 'Hi, pidor ;)')
+   	
    	elsif message.text =~ /https:\/\/www\.youtube\.com/
    		# Launchy.open(message.text)
-   		uri = URI(message.text)
-   		# req = Net::HTTP::Get.new(uri)
-   		response = Net::HTTP.get(uri)
-   		track_name = response[/meta\s?name\s?=\s?.title.\s?content\s?=\s?"(.*?)"/, 1]
-   		binding.pry
-   		Track.create(name: track_name, url: message.text) unless Track.find_by(url: message.text)
+   		Track.create(name: get_track_name(message.text), url: message.text) unless Track.find_by(url: message.text)
+   	
    	else
     	bot.api.send_message(chat_id: message.chat.id, text: 'Я тебя не понимаю :(')
    	end
